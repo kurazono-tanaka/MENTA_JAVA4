b2f13aa55e05f51488c52f9a0e9e42006f20ea43
倉園あずさ <kuraenazusa@kuraenazusanoMacBook-Air.local>
2021-05-28 23:49:31 +0900

    質問依頼

--- js/main.js
+++ js/main.js
@@ -1,41 +1,56 @@
 'use strict';
 
 {
-  let count = 0;
-  const task = document.getElementById('task');
+  const addTaskValue = document.getElementById('task');
   const tbody = document.querySelector('tbody');
 
+  const todo = {};
+  const todos = [];
+
+  // todoを追加する関数
+  function addTask(task) {
+    todo.task = task;
+    todo.status = '作業中';
+    todos.push(todo);
+  }
+
+  // todoを表示する関数
+  function displayTodos() {
+    while(tbody.firstChild !== null){
+      tbody.removeChild(tbody.firstChild);
+    }
+    todos.forEach((todo, index) => {
+      //行作成
+      const tr = document.createElement('tr');
+      //ID
+      const idTd = document.createElement('td');
+      idTd.textContent = index;
+      tr.appendChild(idTd);
+      //コメント
+      const commentTd = document.createElement('td');
+      commentTd .textContent = todo.task;
+      tr.appendChild(commentTd);
+      //状態
+      const statusTd = document.createElement('td');
+      const statusButton = document.createElement('button');
+      statusButton.textContent = todo.status;
+      statusTd.appendChild(statusButton);
+      tr.appendChild(statusTd);
+      //削除
+      const removeTd = document.createElement('td');
+      const removeButton = document.createElement('button');
+      removeButton.textContent = '削除';
+      removeTd.appendChild(removeButton);
+      tr.appendChild(removeTd);
+      //行追加
+      tbody.appendChild(tr);
+    });
+  }
+
   document.getElementById('add').addEventListener('click', () => {
-    const tr = document.createElement('tr');
-    const td = document.createElement('td');
-
-    //ID
-    const idTd = document.createElement('td');
-    idTd .textContent = count;
-    tr.appendChild(idTd);
-
-    //コメント
-    const commentTd = document.createElement('td');
-    commentTd .textContent = task.value;
-    tr.appendChild(commentTd);
-
-    //状態
-    const statusTd = document.createElement('td');
-    const statusButton = document.createElement('button');
-    statusButton.textContent = '作業中';
-    statusTd.appendChild(statusButton);
-    tr.appendChild(statusTd);
-
-    //削除ボタン
-    const removeTd = document.createElement('td');
-    const removeButton = document.createElement('button');
-    removeButton.textContent = '削除';
-    removeTd.appendChild(removeButton);
-    tr.appendChild(removeTd);
-
-    tbody.appendChild(tr);
-
-    task.value = '';
-    count++;
+    const task = addTaskValue.value;
+    addTask(task);
+    displayTodos();
+    addTaskValue.value = '';
   });
 }
